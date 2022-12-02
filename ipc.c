6aa29e92c4b1379ef432ff516eb8a7dc5884c746
Mohammad <mamad.jafari91@gmail.com>
2022-12-02 16:56:42 +0330

    refactor code

--- ipc.c
+++ ipc.c
@@ -28,22 +28,24 @@ int main() {
     char * line = NULL;
     size_t len = 0;
     ssize_t readline;
-    int number_count = 0;
+    int numberCount = 0;
+
 
     fp = fopen("./Practical_Q_1numbers.txt", "r");
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
     while ((readline = getline(&line, &len, fp)) != -1) {
-        number_count ++;
-//        printf("%s", line);
+        numberCount ++;
     }
+
     fclose(fp);
+
     fp = fopen("./Practical_Q_1numbers.txt", "r");
 
-    printf("Number Counts: %d\n", number_count);
+    printf("Number Counts: %d\n", numberCount);
 
-    long a[number_count];
+    long a[numberCount];
     int counter = 0;
 
     while ((readline = getline(&line, &len, fp)) != -1) {
@@ -52,24 +54,14 @@ int main() {
 
     fclose(fp);
 
-
     int numProcess = 32;
-    int sizePerProcess = number_count / numProcess + 1;
-    int fd[2];
+    int sizePerProcess = numberCount / numProcess + 1;
 
-
-    // FIFO file path
     char * myfifo = "/tmp/myfifo";
-
-    // Creating the named file(FIFO)
-    // mkfifo(<pathname>, <permission>)
     mkfifo(myfifo, 0666);
-
-
-
+    
+    int fd[2];
     pipe(fd);
-
-
 
     for (int childP = 0; childP < numProcess; childP++)
     {
@@ -83,10 +75,9 @@ int main() {
             close(fd[0]);
 
             int start = childP * sizePerProcess;
-            int end = min(start + sizePerProcess - 1, number_count - 1);
+            int end = min(start + sizePerProcess - 1, numberCount - 1);
             int pointers[2] = {start, end};
 
-            // send the value on the write-descriptor.
             write(fd[1], &pointers, sizeof(pointers));
 
             close(fd[1]);
@@ -100,28 +91,30 @@ int main() {
             printf("Child Process No [%d], PID [%d], PPID: : [%d], \n", childP, getpid(), getppid());
             printf("Start: [%d], End: [%d], Result:[%ld]\n", pointers[0], pointers[1], result);
             close(fd[0]);
-            int fifo_file = open(myfifo, O_WRONLY);
-            write(fifo_file, &result, sizeof(result));
-            close(fifo_file);
+            int fifoFile = open(myfifo, O_WRONLY);
+            write(fifoFile, &result, sizeof(result));
+            close(fifoFile);
             exit(0);
         }
     }
 
     long subResults[numProcess];
-    for (int i=0 ; i < numProcess; i++){
-	int fifoFile = open(myfifo, O_RDONLY);
-	long result;
-	read(fifoFile, &result, sizeof(result));
-	subResults[i] = result;
-	close(fifoFile);
+    for (int i=0 ; i < numProcess; i++)
+    {
+        int fifoFile = open(myfifo, O_RDONLY);
+        long result;
+        read(fifoFile, &result, sizeof(result));
+        subResults[i] = result;
+        close(fifoFile);
     }
 
     wait(NULL);
-    //int fifo_file = open(myfifo, O_RDONLY);
-    sleep(2);
+    unlink(myfifo);
+    sleep(5);
+
     long result;
     result = recursion(subResults, 0, numProcess-1);
+
     printf("THE FINAL RESULTS is:\n%ld\n", result);
-    unlink(myfifo);
     exit(0);
 }
