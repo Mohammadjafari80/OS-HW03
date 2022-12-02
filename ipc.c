276e82996bc852d0af58e54e56407fdac554afd0
Mohammad <mamad.jafari91@gmail.com>
2022-12-02 12:58:25 +0330

    Add code for functions

--- ipc.c
+++ ipc.c
@@ -1,1 +1,13 @@
-// nothing
+include <sys/wait.h> /* wait */
+#include <stdio.h>
+#include <stdlib.h>   /* exit functions */
+#include <unistd.h>   /* read, write, pipe, _exit */
+#include <string.h>
+
+extern long fun(long a, long b);
+ 
+int main() {
+    int x;
+    int y;
+    printf('%d', fun(x, y));
+}
