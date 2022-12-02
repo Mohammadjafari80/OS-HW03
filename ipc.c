1c64df5f81d8342fd104cb0bd9e0e0b1f519918f
Mohammad <mamad.jafari91@gmail.com>
2022-12-02 13:29:24 +0330

    move to array

--- ipc.c
+++ ipc.c
@@ -14,21 +14,32 @@ int main() {
     char * line = NULL;
     size_t len = 0;
     ssize_t read;
-    int counter = 0;
+    int number_count = 0;
 
     fp = fopen("./Practical_Q_1numbers.txt", "r");
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
     while ((read = getline(&line, &len, fp)) != -1) {
-        counter ++;
+        number_count ++;
+//        printf("%s", line);
+    }
+    fclose(fp);
+    fp = fopen("./Practical_Q_1numbers.txt", "r");
+
+    printf("Number Counts: %d\n", number_count);
+
+    long a[number_count];
+    int counter = 0;
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        a[number_count ++] = strtol(line, NULL, 10);;;
 //        printf("%s", line);
     }
 
-    printf("%d", counter);
-
+    printf("%ld\n", a[0])
+    
     fclose(fp);
     if (line)
         free(line);
-    exit(EXIT_SUCCESS);
 }
