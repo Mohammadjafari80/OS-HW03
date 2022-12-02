d0db690a53c72e23de4e5081686fb9e41252c917
Mohammad <mamad.jafari91@gmail.com>
2022-12-02 13:14:50 +0330

    add read code

--- ipc.c
+++ ipc.c
@@ -7,7 +7,25 @@
 extern long fun(long a, long b);
  
 int main() {
-    long x = 8;
-    long y = 4;
-    printf("%ld", fun(x, y));
+    // long x = 8;
+    // long y = 4;
+    // printf("%ld", fun(x, y));
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen("./Practical_Q_1numbers.txt", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s", line);
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
 }
